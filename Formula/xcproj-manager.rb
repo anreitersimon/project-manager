72d13d3e654ee367844a9a1621bfde0de9e697cb
Simon Anreiter <simon.anreiter@i-mobility.at>
2017-12-30 22:41:50 +0100

    Update brew to 0.0.1

--- Formula/xcproj-manager.rb
+++ Formula/xcproj-manager.rb
@@ -2,7 +2,7 @@ class XCProjManager < Formula
   desc "Manager that creates xcode-projects supporting micro-features"
   homepage "https://github.com/anreitersimon/project-manager"
   url "https://github.com/anreitersimon/project-manager/archive/0.0.1.tar.gz"
-  sha256 "037a6580a5e7cc0996d6d363ae1c96f063913490e57157b5d5ca5b49bbf86e74"
+  sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
   head "https://github.com/anreitersimon/project-manager.git"
 
   depends_on :xcode
